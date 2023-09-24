fb54822a758f859033ba5ad8a3ec4d1c0bf53dd9
preska <wolfwar@outlook.com>
2023-09-24 19:56:23 +0200

    billboards API list

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -8,6 +8,7 @@ import { Separator } from "@/components/ui/separator";
 import { useParams, useRouter } from "next/navigation";
 import { BillboardColumn, columns } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
 
 interface BillboardClientProps {
   data: BillboardColumn[];
@@ -24,14 +25,21 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
           title={`Billboards ( ${data.length})`}
           description="Manage billboards for your store"
         />
+
         <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
           <Plus className="mr-2 h-4 w-4" /> Add New
         </Button>
       </div>
+
       <Separator />
+
       <DataTable searchKey="label" columns={columns} data={data} />
 
       <Heading title="API" description="API Calls for Billboards" />
+
+      <Separator />
+
+      <ApiList entityName="billboards" entityIdName="billboardId" />
     </>
   );
 };
