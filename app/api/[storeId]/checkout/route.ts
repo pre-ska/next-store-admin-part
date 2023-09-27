fdbc1a00ecd1335c5b8c0453ff31ce449db798d1
preska <wolfwar@outlook.com>
2023-09-27 14:08:46 +0200

    stripe webhook and dashboard

---
+++ app/api/[storeId]/checkout/route.ts
@@ -0,0 +1,91 @@
+import Stripe from "stripe";
+import { NextResponse } from "next/server";
+
+import { stripe } from "@/lib/stripe";
+import prismadb from "@/lib/prismadb";
+
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+export async function OPTIONS() {
+  return NextResponse.json({}, { headers: corsHeaders });
+}
+
+// ! ovo je API ruta za checkout na frontendu (cart feature)
+// ! u "summary" fileu, metoda "onCheckout" šalje niz "productIds"
+export async function POST(req: Request, { params }: { params: { storeId: string } }) {
+  const { productIds } = await req.json();
+
+  if (!productIds || productIds.length === 0) {
+    return new NextResponse("Product ids are required", { status: 400 });
+  }
+
+  // ! pronađem sve produkte iz requesta po IDjevima
+  const products = await prismadb.product.findMany({
+    where: {
+      id: {
+        in: productIds,
+      },
+    },
+  });
+
+  const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
+
+  // ! mapiram produkte u array koji stripe prima
+  products.forEach((product) => {
+    line_items.push({
+      quantity: 1,
+      price_data: {
+        currency: "USD",
+        product_data: {
+          name: product.name,
+        },
+        unit_amount: product.price.toNumber() * 100,
+      },
+    });
+  });
+
+  // ! spremim naruđbu sa podacima u moju bazu
+  // pripremim naruđbu sa flagom  isPaid: false
+  const order = await prismadb.order.create({
+    data: {
+      storeId: params.storeId,
+      isPaid: false,
+      orderItems: {
+        create: productIds.map((productId: string) => ({
+          product: {
+            connect: {
+              id: productId,
+            },
+          },
+        })),
+      },
+    },
+  });
+
+  // ! kreiram session na stripe strani, tu ću redirectad usera na frontendu
+  const session = await stripe.checkout.sessions.create({
+    line_items,
+    mode: "payment",
+    billing_address_collection: "required",
+    phone_number_collection: {
+      enabled: true,
+    },
+    success_url: `${process.env.FRONTEND_STORE_URL}/cart?success=1`,
+    cancel_url: `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
+    metadata: {
+      orderId: order.id,
+    },
+  });
+
+  // ! pošaljem frontendu taj session url
+  return NextResponse.json(
+    { url: session.url },
+    {
+      headers: corsHeaders,
+    }
+  );
+}
